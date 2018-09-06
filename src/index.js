d52d3c4945fe88377d41cae7b735a45c49507194
Darya Talanina <dtalanina@gmail.com>
2018-09-06 15:05:57 +0200

    Add counter app

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        count: 0
+    };
+  }
+
+  onClick = () => {
+    this.setState({
+        count: this.state.count + 1
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+          <h1>Counter</h1>
+          <div>{this.state.count}</div>
+          <button onClick={this.onClick}>+1</button>
+      </div>
+    );
+  }
+}
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
