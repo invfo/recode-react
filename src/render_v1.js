1ab5867ed1d34f8fb4196c619512ba543ee0c677
Darya Talanina <dtalanina@gmail.com>
2018-10-10 22:09:24 +0200

    Fix rendering for components

---
+++ src/render_v1.js
@@ -0,0 +1,35 @@
+const render = (vdom, parent) => {
+  if (typeof(vdom) === 'object') {
+
+    let node;
+
+    if (typeof(vdom.type) === 'string') {
+      node = document.createElement(vdom.type);
+    } else if (typeof(vdom.type) === 'function') {
+      node = Component.render(vdom, parent);
+    }
+
+    // gestion des props
+    Object.keys(vdom.props).map((prop) => {
+      if (prop === 'className') {
+        node.className = vdom.props[prop];
+      } else if (prop === 'onClick') {
+        node.addEventListener('click', vdom.props[prop]);
+      }
+    })
+
+    // gestion des enfants
+    for (const child of [].concat(...vdom.children)) {
+      render(child, node);
+    }
+
+    // ajout au DOM
+    parent.appendChild(node);
+    return node;
+  }
+  else if (typeof(vdom) === 'string' || typeof vdom === 'number') {
+    const textNode = document.createTextNode(vdom);
+    parent.appendChild(textNode);
+    return textNode;
+  }
+};
