29f836f7721f5af0713f5cdf5e968c321a386783
Darya Talanina <dtalanina@gmail.com>
2018-10-12 11:56:46 +0200

    .

--- src/component.js
+++ src/component.js
@@ -6,32 +6,13 @@ class Component {
 
   static render(vdom, parent) {
     const instance = new (vdom.type)(vdom.props); // == new App(props)
-
     const element = instance.render() // == element <div><h1>todos</h1>...</div>
     instance.dom = render(element, parent); // on mémorise le DOM rendu
-
-    instance.componentDidMount();
     return instance.dom;
   }
 
   setState(nextState) {
-    if (this.shouldComponentUpdate(this.props, nextState)) {
-      const prevState = this.state;
       this.state = nextState;
-
       patch(this.dom, this.render());
-
-      this.componentDidUpdate(this.props, prevState);
-    } else {
-      this.state = nextState;
-    }
   }
-
-  shouldComponentUpdate(nextProps, nextState) {
-    return this.props != nextProps || this.state != nextState;
-  }
-
-  componentDidMount() {}
-
-  componentDidUpdate() {}
 }
